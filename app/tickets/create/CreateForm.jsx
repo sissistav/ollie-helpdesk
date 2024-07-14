1abc91ff56bc99297fb15adb079d98f6358bb83c
sissi-stav <sissi.stav@gmail.com>
2024-07-14 16:23:03 +0300

    minor changes

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -11,7 +11,7 @@ export default function CreateForm() {
   const [priority, setPriority] = useState('low')
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleSubmit = async (e)  => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     setIsLoading(true)
 
@@ -19,15 +19,17 @@ export default function CreateForm() {
 
     const res = await fetch('http://localhost:4000/tickets', {
       method: "POST",
-      headers: {"Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newTicket)
     })
 
     if (res.status === 201) {
-      router.refresh()
-      router.push('/tickets')
+      const createdTicket = await res.json()
+      router.push(`/tickets/${createdTicket.id}`)
+    } else {
+      // Handle error here if needed
+      setIsLoading(false)
     }
-    
   }
 
   return (
@@ -42,7 +44,7 @@ export default function CreateForm() {
         />
       </label>
       <label>
-        <span>Title:</span>
+        <span>Body:</span>
         <textarea
           required
           onChange={(e) => setBody(e.target.value)}
@@ -64,9 +66,9 @@ export default function CreateForm() {
         className="btn-primary" 
         disabled={isLoading}
       >
-      {isLoading && <span>Adding...</span>}
-      {!isLoading && <span>Add Ticket</span>}
-    </button>
+        {isLoading && <span>Adding...</span>}
+        {!isLoading && <span>Add Ticket</span>}
+      </button>
     </form>
   )
 }
